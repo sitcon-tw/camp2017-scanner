0f56c61445f7f1c453f791bec88f620fb58d8465
yoyo930021 <yoyo930021@gmail.com>
2017-08-11 03:15:52 +0800

    Try to leave telegram

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <qrcode-reader :enable="true" :noResult="true" title="" subTitle="請掃描點數 Qrcode" @OnSuccess="OnSuccess"></qrcode-reader>
+    <qrcode-reader :enable="true" :noResult="true" title="" subTitle="請掃描點數 QRcode" @OnSuccess="OnSuccess"></qrcode-reader>
   </div>
 </template>
 
@@ -12,6 +12,11 @@ export default {
   name: 'app',
   components: {
     QrcodeReader
+  },
+  beforeMount () {
+    if (this.getIsTelegram()) {
+      window.open(window.location.href, '_top')
+    }
   },
   methods: {
     OnSuccess (result) {
@@ -31,6 +36,30 @@ export default {
         }
         return o
       })
+    },
+    getQueryString () {
+      var queryString = {}
+      var query = window.location.search.substring(1)
+      var vars = query.split('&')
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=')
+        // If first entry with this name
+        if (typeof queryString[pair[0]] === 'undefined') {
+          queryString[pair[0]] = decodeURIComponent(pair[1])
+          // If second entry with this name
+        } else if (typeof queryString[pair[0]] === 'string') {
+          var arr = [queryString[pair[0]], decodeURIComponent(pair[1])]
+          queryString[pair[0]] = arr
+          // If third or later entry with this name
+        } else {
+          queryString[pair[0]].push(decodeURIComponent(pair[1]))
+        }
+      }
+      return queryString
+    },
+    getIsTelegram () {
+      var qs = this.getQueryString()
+      return !!qs.inlineId || !!qs.chatId || !!qs.messageId || !!qs.userId
     }
   }
 }
