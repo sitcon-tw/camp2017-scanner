6a00295bb7ebfcedda464859e0b4d67c96d6bc69
yoyo930021 <yoyo930021@gmail.com>
2017-08-11 14:01:33 +0800

    revert

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,6 @@ export default {
     QrcodeReader
   },
   beforeMount () {
-    if (this.getIsTelegram()) {
-      window.open(window.location.href, '_top')
-    }
   },
   methods: {
     OnSuccess (result) {
@@ -36,30 +33,6 @@ export default {
         }
         return o
       })
-    },
-    getQueryString () {
-      var queryString = {}
-      var query = window.location.search.substring(1)
-      var vars = query.split('&')
-      for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=')
-        // If first entry with this name
-        if (typeof queryString[pair[0]] === 'undefined') {
-          queryString[pair[0]] = decodeURIComponent(pair[1])
-          // If second entry with this name
-        } else if (typeof queryString[pair[0]] === 'string') {
-          var arr = [queryString[pair[0]], decodeURIComponent(pair[1])]
-          queryString[pair[0]] = arr
-          // If third or later entry with this name
-        } else {
-          queryString[pair[0]].push(decodeURIComponent(pair[1]))
-        }
-      }
-      return queryString
-    },
-    getIsTelegram () {
-      var qs = this.getQueryString()
-      return !!qs.inlineId || !!qs.chatId || !!qs.messageId || !!qs.userId
     }
   }
 }
