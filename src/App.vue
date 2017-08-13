8ffb7e0374cadaa6a32e2b424edbb0700dd8f24f
yoyo930021 <yoyo930021@gmail.com>
2017-08-14 00:13:54 +0800

    Add god mode and fix generate bug

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,75 @@
 <template>
-  <div id="app">
-    <template v-if="mode === 'student'">
-      <p style="text-align:center;" v-if="isAndroid">如果使用 Android，請點擊上面三個點 開啟於...</p>
-      <qrcode-reader :enable="true" :noResult="true" title="" subTitle="請掃描點數 QRcode" @OnSuccess="OnSuccess"></qrcode-reader>
+  <div id='app'>
+    <template v-if='mode === "student"'>
+      <p style='text-align:center;' v-if='isAndroid'>如果使用 Android，請點擊上面三個點 開啟於...</p>
+      <qrcode-reader :enable='true' :noResult='true' title='' subTitle='請掃描點數 QRcode' @OnSuccess='OnSuccess'></qrcode-reader>
     </template>
-    <template v-else-if="mode === 'admin'">
-      <p style="text-align:center;">{{ (point >= 0) ? '獲得 ': '消耗 ' }}分數：</p>
-      <input type="number" max="1000000" min="-1000000" step="100" list="defaultNumbers" style="text-align:center;width:150px;height:150px;font-size:1.6rem;" v-model="point"/>
-      <datalist id="defaultNumbers">
-        <option value="10000"></option>
-        <option value="1000"></option>
-        <option value="200"></option>
-        <option value="100"></option>
-        <option value="-100"></option>
-        <option value="-200"></option>
-        <option value="-1000"></option>
-        <option value="-10000"></option>
+    <template v-else-if='mode === "admin"'>
+      <p style='text-align:center;'>{{ (point >= 0) ? '獲得 ': '' }}分數：</p>
+      <input type='number' max='500' min='-500' step='10' value='0' list='defaultNumbers' style='text-align:center;width:150px;height:150px;font-size:1.6rem;' v-model='point' />
+      <datalist id='defaultNumbers'>
+        <option value='100'></option>
+        <option value='50'></option>
+        <option value='20'></option>
+        <option value='10'></option>
+        <option value='-10'></option>
+        <option value='-20'></option>
+        <option value='-50'></option>
+        <option value='-100'></option>
       </datalist>
       <p>
-        <select v-model="description">
-          <option>勇敢探索攤位，故{{ ((point >= 0) ? '獲得 ': '消耗 ')  }}</option>
-          <option>因上課表現卓越，{{ ((point >= 0) ? '獲得 ': '消耗 ')  }}</option>
-          <option>無故鬧事，{{ ((point >= 0) ? '獲得 ': '消耗 ')  }}</option>
-          <option>不知道為什麼 我就是想，故{{ ((point >= 0) ? '獲得 ': '消耗 ')  }}</option>
-          <option>活動認真參與，{{ ((point >= 0) ? '獲得 ': '消耗 ')  }}</option>
+        <select v-model='description'>
+          <option>認真參與活動，{{ ((point >= 0) ? '獲得 ': '') }}</option>
+          <option>勇敢探索攤位，{{ ((point >= 0) ? '獲得 ': '') }}</option>
+          <option>上課表現卓越，{{ ((point >= 0) ? '獲得 ': '') }}</option>
+          <option>無故鬧事，{{ ((point >= 0) ? '獲得 ': '') }}</option>
           <option>自訂</option>
         </select>
       </p>
-      <p>
-        <input v-if="description === '自訂'" type="text" v-model="custom" />{{ ((point >= 0) ? '獲得 ': '消耗 ')  }}
+      <p v-if='description == "自訂"'>
+        <input type='text' v-model='custom' />{{ ((point >= 0) ? '獲得 ': '') }}
       </p>
-      <button type="submit" @click="generate">產生</button>
+      <button type='submit' @click='generate'>產生</button>
     </template>
-    <template v-else-if="mode === 'admin-finish'">
-      <canvas id="qrcode" height="240" width="240"></canvas>
-      <p><button @click="back">返回</button></p>
+    <template v-else-if='mode === "admin-finish"'>
+      <canvas id='qrcode' height='240' width='240'></canvas>
+      <p>
+        <button @click='back'>返回</button>
+      </p>
+    </template>
+    <template v-else-if='mode === "god"'>
+      <div style='margin: 0 auto;'>
+        <div style="display: inline-block;margin-right:24px;">
+          <p>解題狀況</p>
+          <table>
+            <tr v-for='(item,index) in problems' :key="'pro' + index">
+              <td>{{ '第' + (index + 1) + '題' }}</td>
+              <td></td>
+              <td>{{ item.solved_team.length }}</td>
+            </tr>
+          </table>
+        </div>
+        <div style="display: inline-block;margin-left:24px;">
+          <p>各組解題狀況</p>
+          <table style='margin: 0 auto;'>
+            <tr v-for='(value, key) in groupProblem' :key="key">
+              <td>{{ key }}</td>
+              <td></td>
+              <td>{{ value }}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </template>
     <template v-else>
-      <table style="margin: 0 auto;">
-        <tr v-for="item in status" :key="item['group_id']">
+      <table style='margin: 0 auto;'>
+        <tr v-for='item in status' :key='item["group_id"]'>
           <td>{{ item.name }}</td>
           <td>{{ item.coin }}</td>
         </tr>
       </table>
     </template>
-    
-    
+  
   </div>
 </template>
 
@@ -69,34 +93,88 @@ export default {
       custom: '',
       coupon: '',
       status: [],
-      lock: false
+      lock: false,
+      group: [
+        {
+          'groupId': -1001134159252,
+          'name': '第一小隊'
+        },
+        {
+          'groupId': -1001137253117,
+          'name': '第二小隊'
+        },
+        {
+          'groupId': -1001109548642,
+          'name': '第三小隊'
+        },
+        {
+          'groupId': -1001099511354,
+          'name': '第四小隊'
+        },
+        {
+          'groupId': -1001110498737,
+          'name': '第五小隊'
+        },
+        {
+          'groupId': -1001083357529,
+          'name': '第六小隊'
+        },
+        {
+          'groupId': -1001110748486,
+          'name': '第七小隊'
+        },
+        {
+          'groupId': -1001113956597,
+          'name': '第八小隊'
+        },
+        {
+          'groupId': -1001128884986,
+          'name': '開發小隊'
+        }
+      ],
+      problems: []
     }
   },
   beforeMount () {
+    var config = {
+      baseURL: 'https://ccip.coscup.org/',
+      timeout: 3000
+    }
+    this.api = axios.create(config)
+
     if ((this.parameters().token || '').length !== 0) {
       this.mode = 'admin'
     } else if ((this.parameters().id || '').length !== 0) {
       this.mode = 'student'
       this.isAndroid = navigator.userAgent.match(/Android/i)
+    } else if ((this.parameters().god || '').length !== 0) {
+      this.mode = 'god'
+      window.setInterval(function () { this.getProblem() }.bind(this), 1000)
     } else {
       this.mode = 'dashboard'
+      window.setInterval(function () { this.getStatus() }.bind(this), 1000)
     }
-    var config = {
-      baseURL: 'https://ccip.coscup.org/',
-      timeout: 3000
-      // headers: {'Content-Type': 'multipart/form-data'}
-    }
-    this.api = axios.create(config)
-
-    window.setInterval(this.getStatus(), 1000)
   },
   computed: {
     desc: function () {
-      if (this.description !== '') {
+      if (this.description !== '自訂') {
         return this.description
       } else {
-        return this.custom + ((this.point >= 0) ? '獲得 ' : '消耗 ')
+        return this.custom + ((this.point >= 0) ? '獲得 ' : ' ')
       }
+    },
+    groupProblem: function () {
+      var result = {}
+      for (var i = 0; i < this.problems.length; i++) {
+        for (var j = 0; j < this.problems[i]['solved_team'].length; j++) {
+          if (result[this.id2GroupName(this.problems[i]['solved_team'][j])] === undefined) {
+            result[this.id2GroupName(this.problems[i]['solved_team'][j])] = 1
+          } else {
+            result[this.id2GroupName(this.problems[i]['solved_team'][j])]++
+          }
+        }
+      }
+      return result
     }
   },
   methods: {
@@ -105,7 +183,7 @@ export default {
         var self = this
         if ((this.parameters().id || '').length !== 0) {
           this.lock = true
-          this.api.post('consume', qs.stringify({group_id: this.parameters().id, coupon: result})).then(function (res) {
+          this.api.post('consume', qs.stringify({ group_id: this.parameters().id, coupon: result })).then(function (res) {
             alert(res.data.status)
             self.lock = false
           }).catch(function (error) {
@@ -150,6 +228,21 @@ export default {
       this.api.get('status').then(function (res) {
         self.status = res.data
       })
+    },
+    getProblem () {
+      var self = this
+      this.api.get('keyword_status').then(function (res) {
+        self.problems = res.data
+      })
+    },
+    id2GroupName (id) {
+      var result = this.group.filter(function (element) {
+        if (element.groupId === id) return true
+        else false
+      })
+      // console.log(result[0].name)
+      if (result.length !== 0) return result[0].name
+      else return ''
     }
   }
 }
